cc3f5c8083e6dd64ffa895691470ccd304c36868
Joseph Chalabi <chalabi.joseph@gmail.com>
2023-07-29 14:42:58 -0700

    fix route for pages

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
           <div className="main-body">
             <Suspense fallback={<Loading />}>
               <Routes>
-                <Route path="/" key={"home"} element={<HomePage />} />
+                <Route path="/althea-app" key={"home"} element={<HomePage />} />
                 <Route
                   path="/bridge/walkthrough"
                   key={"guide"}
