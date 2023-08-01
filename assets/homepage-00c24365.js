54b625d0e97aa11ec6c36e65eff55eaeb2d12eb7
chalabi2 <chalabi2@users.noreply.github.com>
2023-08-01 19:03:17 +0000

    deploy: 796602a9f8f5629d9d20193a07abd49173d1fb78

--- assets/homepage-00c24365.js
+++ assets/homepage-00c24365.js
@@ -1,4 +1,4 @@
-import{n as r,j as t,F as i,a as o,aZ as g,D as p,aI as h,bh as u,bi as m,U as b,T as f}from"./index-18c85320.js";import{H as k}from"./seo-ada6cef7.js";const x="/althea-app/assets/bg-5f337ca0.jpg",w=r.span`
+import{n as r,j as t,F as i,a as o,aZ as g,D as p,aI as h,bh as u,bi as m,U as b,T as f}from"./index-70d811cf.js";import{H as k}from"./seo-6bd940f7.js";const x="/althea-app/assets/bg-5f337ca0.jpg",w=r.span`
 font-family: 'Macan';
 `,v=r.span`
 font-family: 'MacanFont';
