eaa501a52da3803a8ba380fbdc921a424a61350e
chalabi2 <chalabi2@users.noreply.github.com>
2023-08-22 21:26:01 +0000

    deploy: 6761c8283c63b9dc6b1dd895513a761ba747a543

--- assets/proposal-c1e45933.js
+++ assets/proposal-c1e45933.js
@@ -1,4 +1,4 @@
-import{a as d,e as k,b as A,d as z,f as J,V as P,q as W,P as D,c as E}from"./Styled-e9eaef75.js";import{n as C,D as p,a as r,j as t,av as K,J as Q,T as y,P as U,aG as S,H as u,u as v,i as V,B as F,aH as Z,aI as X,aJ as ee,aK as te,aL as oe,f as re,aM as ae,m as ne,R as se,aN as ie,I as le,F as O,ap as ce,Y as de}from"./index-d9575a52.js";import{m as ue,h as pe}from"./transactionHelpers-cd86ce93.js";import{T as he,u as ve,O as me}from"./index-30e50e3e.js";import"./moment-7785d891.js";const T={quorum:"33.4%",threshold:"50%",veto:"33.4%"},ge=C.div`
+import{a as d,e as k,b as A,d as z,f as J,V as P,q as W,P as D,c as E}from"./Styled-f5270f30.js";import{n as C,D as p,a as r,j as t,av as K,J as Q,T as y,P as U,aG as S,H as u,u as v,i as V,B as F,aH as Z,aI as X,aJ as ee,aK as te,aL as oe,f as re,aM as ae,m as ne,R as se,aN as ie,I as le,F as O,ap as ce,Y as de}from"./index-f4e56ad3.js";import{m as ue,h as pe}from"./transactionHelpers-2b290f75.js";import{T as he,u as ve,O as me}from"./index-d135049a.js";import"./moment-7785d891.js";const T={quorum:"33.4%",threshold:"50%",veto:"33.4%"},ge=C.div`
   background-color: var(--base);
   height: min-content;
   width: 26rem;
