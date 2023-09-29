3eb1d41604b25e49d9b0fa22497a94ca211bf1ed
Joseph Chalabi <100090645+chalabi2@users.noreply.github.com>
2023-09-29 09:10:59 -0700

    change network

--- src/global/config/networks.ts
+++ src/global/config/networks.ts
@@ -62,7 +62,7 @@ export const AltheaMainnet: AltheaNetwork = {
   },
   icon: altheaIcon,
   chainId: 417834,
-  rpcUrl: "https://nodes.chandrastation.com/testnet/evm/althea/",
+  rpcUrl: "http://66.172.36.142:3845",
   isTestChain: false,
   isLocalChain: false,
   multicallAddress: "0x5785e0027a8c937627C01C9EB5F543bA42f8BB6b",
@@ -73,7 +73,7 @@ export const AltheaMainnet: AltheaNetwork = {
   //althea specific
   coreContracts: CORE_ADDRESSES.AltheaMainnet,
   cosmosBlockExplorerUrl: "https://www.mintscan.io/althea",
-  cosmosAPIEndpoint: "https://nodes.chandrastation.com/testnet/api/althea/",
+  cosmosAPIEndpoint: "http://66.172.36.142:3817",
   cosmosChainId: "althea_417834-3",
 };
 export const ETHMainnet: ETHBridgeNetwork = {
